8cf02db470cab4d9e3f09a2d475af7ee124fdf42
IvanGv12 <ivangomva13@gmail.com>
2024-12-09 19:04:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ const PORT = process.env.PORT || 3977;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
 app.get("/", (req, res) => {
-    res.status(200).send({ msg: "HOLA TINCODE" });
+    res.status(200).send({ 
+        Nombre: "Héctor Iván Gómez Vázquez", 
+        NumeroControl: "22040376" 
+    });
 });
+
 
 app.post("/welcome", (req, res) => {
     const { username } = req.body;
     res.status(200).send({ msg: `Hola, ${username}` });
 });
+
 
 app.listen(PORT, () => {
     console.log(`Tu servidor está listo en el puerto ${PORT}`);
